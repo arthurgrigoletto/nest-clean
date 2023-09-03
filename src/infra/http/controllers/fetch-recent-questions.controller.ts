76451493a1f1837df98978511ba94cbcd9feed50
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2023-09-03 09:50:50 -0300

    Implementando respositorios do prisma

--- src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -8,7 +8,7 @@ import {
 } from '@nestjs/common'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 import { z } from 'zod'
 
 const pageQueryParamSchema = z
