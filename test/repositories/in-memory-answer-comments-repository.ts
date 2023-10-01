c72bd7c7bdf4f636158af513124aedc82fbcb03a
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2023-10-01 18:38:17 -0300

    Comentario da resposta com autor

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,16 +1,51 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
 
 export class InMemoryAnswerCommentsRepository
   implements AnswerCommentsRepository
 {
   public items: AnswerComment[] = []
 
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
+
   async findManyByAnswerId(answer: string, { page }: PaginationParams) {
     const answerComments = this.items
       .filter((item) => item.answerId.toString() === answer)
       .slice((page - 1) * 20, page * 20)
+
+    return answerComments
+  }
+
+  async findManyByAnswerIdWithAuthor(
+    answerId: string,
+    { page }: PaginationParams,
+  ) {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+      .map((comment) => {
+        const author = this.studentsRepository.items.find((student) => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()}" does not exists.`,
+          )
+        }
+
+        return CommentWithAuthor.create({
+          content: comment.content,
+          commentId: comment.id,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+          authorId: author.id,
+          author: author.name,
+        })
+      })
 
     return answerComments
   }
